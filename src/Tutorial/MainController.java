e245986b8ebae2c45b5a429a82a691de0ec96a0c
Steven Taylor <steven.taylor81@gmail.com>
2018-09-28 17:18:57 +0100

    ComboBox example added

--- src/Tutorial/MainController.java
+++ src/Tutorial/MainController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 
 import java.net.URL;
@@ -21,19 +22,34 @@ public class MainController implements Initializable {
     @FXML private ChoiceBox choiceBox;
     @FXML private Label choiceBoxLabel;
 
+    //These items are for the ComboBox
+    @FXML private ComboBox comboBox;
+    @FXML private Label comboBoxLabel;
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         pizzaToppingsLabel.setText("");
 
+        //These items are for the ChoiceBox
         choiceBoxLabel.setText("");
         choiceBox.getItems().add("Apples");
         choiceBox.getItems().add("Bananas");
         choiceBox.getItems().addAll("Oranges","Pears","Strawberries");
         choiceBox.setValue("Apples");
+
+        //These items are for the ComboBox
+        comboBox.getItems().add("COMP1030");
+        comboBox.getItems().addAll("COMP1008","MGMT2003","MGMT2010");
+        comboBoxLabel.setText("");
+    }
+
+    public void comboBoxWasUpdated(){
+        comboBoxLabel.setText("Course selected:\n" + comboBox.getValue().toString());
     }
 
     public void choiceBoxButtonPressed(){
-        choiceBoxLabel.setText("My favourite fruit is\n"+choiceBox.getValue().toString());
+        choiceBoxLabel.setText("My favourite fruit is\n" + choiceBox.getValue().toString());
     }
 
     public void pizzaOrderButtonPressed(){
